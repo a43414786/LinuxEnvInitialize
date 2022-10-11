d5a91571de0d360696f5e56b399c025a768f231f
a43414786 <72087693+a43414786@users.noreply.github.com>
2022-10-11 20:40:56 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt upgrade
+
+#Install git
+sudo apt install git
+#Install vim
+sudo apt install git curl vim-gtk3
+#Install gcc
+sudo apt install build-essential
+gcc --version
+#Install plugin manager “vim-plug”
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vimplug/master/plug.vim
